4c780b7f1ea08975ecc1f160748ee6fb7af14ab0
sheblight <bkandwekid@gmail.com>
2025-01-03 16:21:09 -0500

    Initial commit

---
+++ SwissKnife.py
@@ -0,0 +1,17 @@
+"""
+Written by pow
+
+Hobby project for building easy toolsets.
+This is the main class for swissknife program.
+This acts as the display for accessing different modules.
+
+"""
+
+def main():
+    print("bitch")
+
+
+# Using the special variable 
+# __name__
+if __name__=="__main__":
+    main()
